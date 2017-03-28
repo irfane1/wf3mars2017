7d08d942d2ed98fa61f4bcdb89eddfc746c5dd32
Etudiant <Etudiant>
2017-03-28 12:35:20 +0200

    28mars

---
+++ 28mars/exo7/js/script.js
@@ -0,0 +1,21 @@
+// Créer une fonction sans paramètres
+function helloWorld(){
+
+    // Ecrire le code à exécuter à l'appel de la fonction
+    console.log('Bonjour le monde');
+
+};
+
+// Appeler la fonction
+helloWorld();
+
+
+// Créer une fonction avec paramètres
+function fullName( firstName, lastName ){
+
+    console.log('Bonjour ' + firstName + ' ' + lastName);
+
+};
+
+// Appeler la fonction en précisant les paramètres
+fullName( 'Irfane', 'Tayabdjee' );
