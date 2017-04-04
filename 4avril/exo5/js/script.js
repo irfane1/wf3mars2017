06140ea7b69f5a3e756bcbc6683868b2d8b4c47e
Etudiant <Etudiant>
2017-04-04 17:16:45 +0200

    4avril

---
+++ 4avril/exo5/js/script.js
@@ -0,0 +1,23 @@
+// Attendre le chargement du DOM
+$(document).ready(function(){
+
+    
+   // Burger menu classique
+   $('.fa-bars').click(function(){
+
+        $('nav ul').fadeIn(500);
+
+   });
+
+   // Fermer le Burger menu classique
+   $('a').click(function(evt){
+
+        // Bloquer la balise a
+        evt.preventDefault();
+
+        $('nav ul').fadeOut(500);
+
+   });
+
+
+}); // Fin de la fonction d'attente de chargement du DOM
